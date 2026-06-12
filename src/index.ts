6a80864783fc04cbb37b0b46ebec0df26b49a034
Ichsan Laptop <cangcodes@gmail.com>
2026-06-12 19:54:20 +0800

    update database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config';
-
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
-    const client = postgres(process.env.DATABASE_URL!)
-    export const db = drizzle({ client });
+const connectionString = process.env.DATABASE_URL!
 
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(connectionString, { prepare: false })
+export const db = drizzle(client);
